2920585f9a7c6b0f16468d8dfb0292415e795ccb
Stanislau Marudau <marudov.stas@gmail.com>
2023-08-30 19:13:49 +0200

    Parser implemented

--- Parser.java
+++ Parser.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+
 public class Parser {
-    
+    public ArrayList<Item> parse(String input) {
+        String[] inputSplit = input.split(" ");
+        ArrayList<Item> expr = new ArrayList<Item>();
+        for (String each : inputSplit) {
+            switch (each) {
+                case "*":
+                    expr.add(Item.Mul());
+                    break;
+                case "-":
+                    expr.add(Item.Sub());
+                    break;
+                case "+":
+                    expr.add(Item.Add());
+                    break;
+                case "/":
+                    expr.add(Item.Div());
+                    break;
+                default:
+                    expr.add(Item.Value(Integer.parseInt(each)));
+                    break;
+            }
+        }
+        return expr;
+    }
 }
