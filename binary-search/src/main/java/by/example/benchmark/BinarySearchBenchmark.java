a94495eab7b16721159a665c1152a064542a98a5
Stanislau Marudau <marudov.stas@gmail.com>
2023-09-06 23:03:42 +0200

    Improved binary search benchmark

---
+++ binary-search/src/main/java/by/example/benchmark/BinarySearchBenchmark.java
@@ -0,0 +1,81 @@
+package by.example.benchmark;
+
+import by.example.impl.Binary;
+import by.example.impl.Linear;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class BinarySearchBenchmark {
+    private static final int LOOP = 1000;
+    @Param({"100", "200", "400", "800", "1600", "3200", "6400", "12800", "25600"})
+    private int n;
+    private int[] DATA_FOR_TESTING;
+    private int[] KEYS;
+
+    public static void main(String[] args) throws RunnerException {
+
+        Options opt = new OptionsBuilder()
+                .include(BinarySearchBenchmark.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Setup
+    public void setup() {
+        DATA_FOR_TESTING = createData();
+        KEYS = createKeys();
+    }
+
+    @Benchmark
+    public void binarySearch(Blackhole bh) {
+        int i = 0;
+        while (i < KEYS.length) {
+            boolean res = Binary.search(DATA_FOR_TESTING, KEYS[i]);
+            bh.consume(res);
+            i++;
+        }
+    }
+
+    @Benchmark
+    public void linearSearch(Blackhole bh) {
+        int i = 0;
+        while (i < KEYS.length) {
+            boolean res = Linear.search(DATA_FOR_TESTING, KEYS[i]);
+            bh.consume(res);
+            i++;
+        }
+    }
+
+    private int[] createData() {
+        Random rnd = new Random();
+        int[] array = new int[n];
+        int nxt = rnd.nextInt(10);
+
+        for (int i = 0; i < n; i++) {
+            array[i] = nxt;
+            nxt += rnd.nextInt(10) + 1;
+        }
+        return array;
+    }
+
+    private int[] createKeys() {
+        Random rnd = new Random();
+        int[] keys = new int[LOOP];
+        for (int i = 0; i < LOOP; i++) {
+            keys[i] = rnd.nextInt(n * 5);
+        }
+        return keys;
+    }
+}
