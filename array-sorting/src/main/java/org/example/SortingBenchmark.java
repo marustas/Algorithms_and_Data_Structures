3f0adc725913fa0765c36c78e1c63289b38aee1f
Stanislau Marudau <marudov.stas@gmail.com>
2023-09-12 14:56:39 +0200

    Merge sort implemented

--- array-sorting/src/main/java/org/example/SortingBenchmark.java
+++ array-sorting/src/main/java/org/example/SortingBenchmark.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class SortingBenchmark {
     private static int[] createData(int n) {
         Random rnd = new Random();
-        int [] array = new int[n];
+        int[] array = new int[n];
         for (int a = 0; a < array.length; a++) {
             array[a] = rnd.nextInt(n * 10);
         }
@@ -13,20 +13,20 @@ public class SortingBenchmark {
     }
 
     public static void main(String[] arg) {
-int[] sizes = {5, 10, 15, 20};
-for(int s: sizes){
-    int[] array = createData(s);
+        int[] sizes = {5, 10, 15, 20};
+        for (int s : sizes) {
+            int[] array = createData(s);
 
-    System.out.println("BEFORE SORTING:\n");
-    for (int j = 0; j < s; j++) {
-        System.out.println(array[j]);
-    }
+            System.out.println("BEFORE SORTING:\n");
+            for (int j = 0; j < s; j++) {
+                System.out.println(array[j]);
+            }
 
-    System.out.println("AFTER SORTING:\n");
-    InsertionSort.sort(array);
-    for (int j = 0; j < s; j++) {
-        System.out.println(array[j]);
-    }
-}
+            System.out.println("AFTER SORTING:\n");
+            InsertionSort.sort(array);
+            for (int j = 0; j < s; j++) {
+                System.out.println(array[j]);
+            }
+        }
     }
 }
