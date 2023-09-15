bdf6a663491476ec5589d51bdaf6ed6542b9f52e
Stanislau Marudau <marudov.stas@gmail.com>
2023-09-15 19:41:32 +0200

    Polishing up part 2

--- array-sorting/src/test/java/by/example/SortingConsoleBenchmark.java
+++ array-sorting/src/test/java/by/example/SortingConsoleBenchmark.java
@@ -1,48 +1,48 @@
 package by.example;
 
-import static by.example.TestDataUtils.createData;
-
 import by.example.sort.Insertion;
 import by.example.sort.Merge;
 import by.example.sort.Selection;
 import by.example.sort.Sortable;
 
+import static by.example.TestDataUtils.createData;
+
 public class SortingConsoleBenchmark {
 
-	public static void main(String[] arg) {
-		int[] sizes = { 1_000, 10_000 };
-		int loop = 10;
-		int tries = 100;
-		System.out.printf("Number of elements\t Selection time\t Insertion time\t Merge time\t Ratio%n");
-		for (int s : sizes) {
+    public static void main(String[] arg) {
+        int[] sizes = {1_000, 10_000};
+        int loop = 10;
+        int tries = 100;
+        System.out.printf("Number of elements\t Selection time\t Insertion time\t Merge time\t Ratio%n");
+        for (int s : sizes) {
 
-			int[] array = createData(s);
+            int[] array = createData(s);
 
-			var divisor = loop * 1000;
-			double ratio = (getMinExecutionTime(new Insertion(), tries, loop, array) / divisor) / (getMinExecutionTime(new Selection(), tries, loop, array) / divisor);
-			System.out.printf("%d\t\t\t\t%8.0f\t\t\t%8.0f\t\t%8.0f\t%.2f\n",
-					s,
-					getMinExecutionTime(new Selection(), tries, loop, array) / divisor,
-					getMinExecutionTime(new Insertion(), tries, loop, array) / divisor,
-					getMinExecutionTime(new Merge(), tries, loop, array) / divisor,
-					ratio);
+            var divisor = loop * 1000;
+            double ratio = (getMinExecutionTime(new Insertion(), tries, loop, array) / divisor) / (getMinExecutionTime(new Selection(), tries, loop, array) / divisor);
+            System.out.printf("%d\t\t\t\t%8.0f\t\t\t%8.0f\t\t%8.0f\t%.2f\n",
+                    s,
+                    getMinExecutionTime(new Selection(), tries, loop, array) / divisor,
+                    getMinExecutionTime(new Insertion(), tries, loop, array) / divisor,
+                    getMinExecutionTime(new Merge(), tries, loop, array) / divisor,
+                    ratio);
 
-		}
-	}
+        }
+    }
 
-	private static double getMinExecutionTime(Sortable algorithm, int tries, int loop, int[] array) {
-		double min = Double.POSITIVE_INFINITY;
-		for (int i = 0; i < tries; i++) {
-			double selectionStart = System.nanoTime();
-			for (int j = 0; j < loop; j++) {
-				int[] clone = array.clone();
-				algorithm.sort(clone);
-			}
-			double executionTime = System.nanoTime() - selectionStart;
-			if (executionTime < min) {
-				min = executionTime;
-			}
-		}
-		return min;
-	}
+    private static double getMinExecutionTime(Sortable algorithm, int tries, int loop, int[] array) {
+        double min = Double.POSITIVE_INFINITY;
+        for (int i = 0; i < tries; i++) {
+            double selectionStart = System.nanoTime();
+            for (int j = 0; j < loop; j++) {
+                int[] clone = array.clone();
+                algorithm.sort(clone);
+            }
+            double executionTime = System.nanoTime() - selectionStart;
+            if (executionTime < min) {
+                min = executionTime;
+            }
+        }
+        return min;
+    }
 }
--- array-sorting/src/test/java/by/example/SortingJmhBenchmark.java
+++ array-sorting/src/test/java/by/example/SortingJmhBenchmark.java
@@ -1,65 +1,65 @@
 package by.example;
 
-import static by.example.TestDataUtils.createData;
-
-import java.util.concurrent.TimeUnit;
-
 import by.example.sort.ImprovedMerge;
+import by.example.sort.Insertion;
+import by.example.sort.Merge;
+import by.example.sort.Selection;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import by.example.sort.Insertion;
-import by.example.sort.Merge;
-import by.example.sort.Selection;
+import java.util.concurrent.TimeUnit;
+
+import static by.example.TestDataUtils.createData;
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class SortingJmhBenchmark {
-	@Param({ "1000", "2000", "3000", "4000", "5000", "6000", "7000", "8000", "9000", "10000", "11000", "12000", "13000", "14000", "15000", })
-	private int n;
-	private int[] DATA_FOR_TESTING;
+    @Param({"1000", "2000", "3000", "4000", "5000", "6000", "7000", "8000", "9000", "10000", "11000", "12000", "13000", "14000", "15000",})
+    private int n;
+    private int[] DATA_FOR_TESTING;
 
-	public static void main(String[] args) throws RunnerException {
+    public static void main(String[] args) throws RunnerException {
 
-		Options opt = new OptionsBuilder()
-				.include(SortingJmhBenchmark.class.getSimpleName())
-				.forks(1)
-				.threads(1)
-				.warmupIterations(5)
-				.build();
+        Options opt = new OptionsBuilder()
+                .include(SortingJmhBenchmark.class.getSimpleName())
+                .forks(1)
+                .threads(1)
+                .warmupIterations(5)
+                .build();
 
-		new Runner(opt).run();
-	}
+        new Runner(opt).run();
+    }
 
-	@Setup
-	public void setup() {
-		DATA_FOR_TESTING = createData(n);
-	}
+    @Setup
+    public void setup() {
+        DATA_FOR_TESTING = createData(n);
+    }
 
-	@Benchmark
-	public void improvedMergeSort() {
-		int[] clone = DATA_FOR_TESTING.clone();
-		new ImprovedMerge().sort(clone);
-	}
-	@Benchmark
-	public void mergeSort() {
-		int[] clone = DATA_FOR_TESTING.clone();
-		new Merge().sort(clone);
-	}
+    @Benchmark
+    public void improvedMergeSort() {
+        int[] clone = DATA_FOR_TESTING.clone();
+        new ImprovedMerge().sort(clone);
+    }
 
-	@Benchmark
-	public void selectionSort() {
-		int[] clone = DATA_FOR_TESTING.clone();
-		new Selection().sort(clone);
-	}
+    @Benchmark
+    public void mergeSort() {
+        int[] clone = DATA_FOR_TESTING.clone();
+        new Merge().sort(clone);
+    }
 
-	@Benchmark
-	public void insertionSort() {
-		int[] clone = DATA_FOR_TESTING.clone();
-		new Insertion().sort(clone);
-	}
+    @Benchmark
+    public void selectionSort() {
+        int[] clone = DATA_FOR_TESTING.clone();
+        new Selection().sort(clone);
+    }
+
+    @Benchmark
+    public void insertionSort() {
+        int[] clone = DATA_FOR_TESTING.clone();
+        new Insertion().sort(clone);
+    }
 }
