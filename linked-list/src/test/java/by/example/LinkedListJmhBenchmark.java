f157157f35da08fca5b9032608c83ce8d78fd214
Stanislau Marudau <marudov.stas@gmail.com>
2023-09-19 23:24:53 +0200

    Benchmark fully uncommented

--- linked-list/src/test/java/by/example/LinkedListJmhBenchmark.java
+++ linked-list/src/test/java/by/example/LinkedListJmhBenchmark.java
@@ -9,6 +9,9 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.example.LinkedListBenchmark.appendArrays;
+import static org.example.LinkedListBenchmark.createArray;
+
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -16,8 +19,8 @@ public class LinkedListJmhBenchmark {
     @Param({"100", "200", "400", "800", "1600", "3200", "6400", "12800"})
     private int sizeA;
     final private int sizeB = 100;
-    //    private int[] arrayA;
-    //    private int[] arrayB;
+        private int[] arrayA;
+        private int[] arrayB;
     private LinkedList linkedListA;
     private LinkedList linkedListB;
 
@@ -34,29 +37,29 @@ public class LinkedListJmhBenchmark {
 
     @Setup(Level.Invocation)
     public void setup() {
-//        arrayA = createArray(sizeA);
-//        arrayB = createArray(sizeB);
+        arrayA = createArray(sizeA);
+        arrayB = createArray(sizeB);
         linkedListA = new LinkedList(sizeA);
         linkedListB = new LinkedList(sizeB);
     }
 
-    //    @Benchmark
-//    public void appendToGrowingArray() {
-//        int[] cloneA = arrayA.clone();
-//        int[] cloneB = arrayB.clone();
-//        appendArrays(cloneA, cloneB);
-//    }
-//
+        @Benchmark
+    public void appendToGrowingArray() {
+        int[] cloneA = arrayA.clone();
+        int[] cloneB = arrayB.clone();
+        appendArrays(cloneA, cloneB);
+    }
+
     @Benchmark
     public void appendToGrowingList() {
         linkedListA.append(linkedListB);
     }
-//    @Benchmark
-//    public void appendGrowingArray() {
-//        int[] cloneA = arrayA.clone();
-//        int[] cloneB = arrayB.clone();
-//        appendArrays(cloneB, cloneA);
-//    }
+    @Benchmark
+    public void appendGrowingArray() {
+        int[] cloneA = arrayA.clone();
+        int[] cloneB = arrayB.clone();
+        appendArrays(cloneB, cloneA);
+    }
 
     @Benchmark
     public void appendGrowingList() {
