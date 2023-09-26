3fffa619e21990c66b44487153a8d645a4e2ebc1
Stanislau Marudau <marudov.stas@gmail.com>
2023-09-26 14:00:16 +0200

    Implemented add() method for Binary tree

--- binary-tree/src/main/java/by/example/BinaryTree.java
+++ binary-tree/src/main/java/by/example/BinaryTree.java
@@ -1,7 +1,47 @@
 package by.example;
 
 public class BinaryTree {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static class Node {
+        public int key;
+        public int value;
+        public Node left, right;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.left = this.right = null;
+        }
     }
+
+    Node root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public void lookup(int key) {
+
+    }
+
+    public void add(Integer key, Integer value) {
+        root = addRecursive(root, key, value);
+    }
+    private Node addRecursive(Node current, Integer key, Integer value) {
+        if (current == null) {
+            return new Node(key, value);
+        }
+
+        if (key.equals(current.key)) {
+            current.value = value;
+        } else if (key < current.key) {
+            current.left = addRecursive(current.left, key, value);
+        } else {
+            current.right = addRecursive(current.right, key, value);
+        }
+
+        return current;
+    }
+
+
 }
+
---
+++ binary-tree/src/test/java/Benchmark.java
@@ -0,0 +1,12 @@
+import by.example.BinaryTree;
+
+public class Benchmark {
+    public static void main(String[] args){
+        BinaryTree tree = new BinaryTree();
+        tree.add(5,105);
+        tree.add(2,102);
+        tree.add(7,107);
+        tree.add(1,101);
+        System.out.println(tree);
+    }
+}
