149030c1db35b89ec052f0fa9d4ee7da3b04ee85
Stanislau Marudau <marudov.stas@gmail.com>
2023-10-05 12:52:23 +0200

    QuickSort using linked list is working

--- quick-sort/src/main/java/by/example/QuickSortList.java
+++ quick-sort/src/main/java/by/example/QuickSortList.java
@@ -1,37 +1,76 @@
 package by.example;
-
 public class QuickSortList {
-    static LinkedList smallNodes = new LinkedList();
-    static LinkedList largeNodes = new LinkedList();
+    public void sort(LinkedList list) {
+        if (list.first == null || list.first == list.last) {
+            return;
+        }
 
-    public static void sort(LinkedList list) {
-        sortRecursive(list);
-        smallNodes.append(largeNodes);
-    }
-
-    private static void sortRecursive(LinkedList list) {
-        partition(list);
-        sortRecursive(smallNodes);
-        sortRecursive(largeNodes);
-    }
-
-    private static Node partition(LinkedList list) {
+        LinkedList smallNodes = new LinkedList();
+        LinkedList largeNodes = new LinkedList();
 
         Node pivot = list.first;
+        list.first = pivot.next;
+        pivot.next = null;
+
         Node current = list.first;
 
         while (current != null) {
-            while (current.value <= pivot.value) {
-                smallNodes.add(current.value);
-                current = smallNodes.last.next;
+            Node next = current.next;
+            if (current.value <= pivot.value) {
+                current.next = smallNodes.first;
+                smallNodes.first = current;
+            } else {
+                current.next = largeNodes.first;
+                largeNodes.first = current;
             }
-            while (current.value > pivot.value) {
-                largeNodes.add(current.value);
-                current = largeNodes.last.next;
+            current = next;
+        }
+
+        sort(smallNodes);
+        sort(largeNodes);
+
+        list.first = append(smallNodes.first, pivot, largeNodes.first);
+        list.last = findLast(list.first);
+    }
+
+    private Node append(Node small, Node pivot, Node large) {
+        if (small == null) {
+            if (large == null) {
+                return pivot;
+            } else {
+                return append(pivot, large);
             }
+        } else {
+            Node lastSmall = findLast(small);
+            lastSmall.next = pivot;
+            if (large == null) {
+                return small;
+            } else {
+                lastSmall = findLast(large);
+                lastSmall.next = null;
+                return append(small, large);
+            }
+        }
+    }
+
+    private Node append(Node first, Node second) {
+        if (first == null) {
+            return second;
+        }
+
+        Node current = first;
+        while (current.next != null) {
             current = current.next;
         }
-        return pivot;
+        current.next = second;
+        return first;
+    }
+
+    private Node findLast(Node node) {
+        while (node != null && node.next != null) {
+            node = node.next;
+        }
+        return node;
     }
 
     private static class Node {
@@ -49,29 +88,30 @@ public class QuickSortList {
         private Node last;
 
         public void add(int item) {
-            Node newCell;
+            Node newCell = new Node(item, null);
             if (first == null) {
-                last = first = new Node(item, null);
+                last = first = newCell;
             } else {
-                newCell = new Node(item, null);
                 last.next = newCell;
                 last = newCell;
             }
         }
-
-        public void append(LinkedList anotherList) {
-            last.next = anotherList.first;
-            anotherList.first = null;
-        }
-
     }
 
     public static void main(String[] args) {
+        QuickSortList quickSort = new QuickSortList();
         LinkedList list = new LinkedList();
         list.add(6);
         list.add(4);
         list.add(7);
-        sort(list);
-        System.out.println();
+        list.add(2);
+        list.add(9);
+        quickSort.sort(list);
+
+        Node current = list.first;
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
     }
 }
