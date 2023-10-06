c3a8238a3af2f202c963a634fb1c679bd1171e29
Stanislau Marudau <marudov.stas@gmail.com>
2023-10-06 11:30:01 +0200

    Benchmark improved even more

--- quick-sort/src/test/java/QuickSortBenchmark.java
+++ quick-sort/src/test/java/QuickSortBenchmark.java
@@ -15,18 +15,31 @@ public class QuickSortBenchmark {
     }
 
     public static void main(String[] args) {
-        int[] sizes = {100, 200, 400, 800, 1600, 3200, 6400, 12800};
+        int[] sizes = {10, 200, 400, 800, 1600, 3200, 6400, 12800};
         int bound = 100_000;
         int tries = 100_000;
         System.out.println("N\t\tList time\t\tArray time\t\t List function\t\t Array function");
         for (int size : sizes) {
+
+            double min2 = Double.POSITIVE_INFINITY;
             double min1 = Double.POSITIVE_INFINITY;
             for (int t = 0; t < tries; t++) {
+                QuickSortArray quickSortArray = new QuickSortArray();
+                int[] array = createArray(size, bound);
+                int[] clone = array.clone();
+
+                double arrayStart = System.nanoTime();
+                quickSortArray.sort(clone);
+                double arrayTime = System.nanoTime() - arrayStart;
+
+                if (arrayTime < min2) {
+                    min2 = arrayTime;
+                }
+
                 QuickSortList quickSortList = new QuickSortList();
-                Random random = new Random();
                 QuickSortList.LinkedList list = new QuickSortList.LinkedList();
                 for (int i = 0; i < size; i++) {
-                    list.add(random.nextInt(bound));
+                    list.add(array[i]);
                 }
 
                 double listStart = System.nanoTime();
@@ -37,24 +50,9 @@ public class QuickSortBenchmark {
                     min1 = listTime;
                 }
             }
-
-            double min2 = Double.POSITIVE_INFINITY;
-            for (int t = 0; t < tries; t++) {
-                QuickSortArray quickSortArray = new QuickSortArray();
-                int[] array = createArray(size, bound);
-
-                double arrayStart = System.nanoTime();
-                int[] clone = array.clone();
-                quickSortArray.sort(clone);
-                double arrayTime = System.nanoTime() - arrayStart;
-
-                if (arrayTime < min2) {
-                    min2 = arrayTime;
-                }
-            }
-            double functionList = min1 / 1000 / size / Math.log(size);
-            double functionArray = min2 / 1000 / size / Math.log(size);
-            System.out.printf("%d \t\t %.2f \t\t %.2f \t\t %.2f \t\t %.2f\n", size, min1 / 1000, min2 / 1000, functionList, functionArray);
+            double functionList = min1 / size / Math.log(size);
+            double functionArray = min2 / size / Math.log(size);
+            System.out.printf("%d \t\t %.2f \t\t %.2f \t\t\t\t %.2f \t\t\t\t %.2f\n", size, min1 / 1000, min2 / 1000, functionList, functionArray);
         }
 
         System.out.println();
