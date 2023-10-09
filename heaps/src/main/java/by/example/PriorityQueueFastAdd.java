c28c994fa6dfef001dcff30b069ccc1cba2aa6e0
Stanislau Marudau <marudov.stas@gmail.com>
2023-10-09 22:46:24 +0200

    Implemented a PriorityQueue with fast add()

---
+++ heaps/src/main/java/by/example/PriorityQueueFastAdd.java
@@ -0,0 +1,50 @@
+package by.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PriorityQueueFastAdd {
+    private final List<Integer> queue;
+
+    public PriorityQueueFastAdd() {
+        queue = new ArrayList<>();
+    }
+
+    public void add(int value) {
+        queue.add(value);
+    }
+
+    public Integer remove() {
+        if (isEmpty()) {
+            int minIndex = 0;
+            int minValue = queue.get(0);
+
+            for (int i = 1; i < queue.size(); i++) {
+                if (queue.get(i) < minValue) {
+                    minIndex = i;
+                    minValue = queue.get(i);
+                }
+            }
+
+            return queue.remove(minIndex);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean isEmpty() {
+        return !queue.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        PriorityQueueFastAdd pq = new PriorityQueueFastAdd();
+        pq.add(3);
+        pq.add(1);
+        pq.add(4);
+        pq.add(2);
+
+        while (pq.isEmpty()) {
+            System.out.println(pq.remove());
+        }
+    }
+}
