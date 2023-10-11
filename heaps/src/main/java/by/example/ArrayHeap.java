b85f6956368d44b231b834967e6621ca74535ad9
Stanislau Marudau <marudov.stas@gmail.com>
2023-10-11 11:09:13 +0200

    Found a bug in bubble method and fixed it

--- heaps/src/main/java/by/example/ArrayHeap.java
+++ heaps/src/main/java/by/example/ArrayHeap.java
@@ -1,55 +1,47 @@
 package by.example;
 
 public class ArrayHeap {
-    // magic needs to be the last element after the filled spot
+    int size;
+    int capacity;
     int[] heap;
     int root = 0;
-    int left, right;
-    int k;
 
-    public ArrayHeap(int size) {
-        heap = new int[size];
+    public ArrayHeap(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        heap = new int[capacity];
         root = heap[0];
-        k = heap.length - 1;
     }
 
-    public void bubble(int item) {
-        if(heap[root] == 0){
-            heap[root] = item;
-        }else{
-            int parent = item % 2 == 0 ? (item - 2) / 2 : (item - 1) / 2;
-            heap[k] = item;
-            if (heap[k] < heap[parent]) {
-                int temp = heap[parent];
-                heap[parent] = heap[k];
-                heap[k] = temp;
-                bubble(heap[k]);
-            }
+    public void add(int item) {
+        heap[size] = item;
+        int currentIndex = size;
+        int parentIndex = currentIndex % 2 == 0 ? (currentIndex - 2) / 2 : (currentIndex - 1) / 2;
+        size++;
+        while (currentIndex > 0 && heap[currentIndex] < heap[parentIndex]) {
+            swap(currentIndex, parentIndex);
+            currentIndex = parentIndex;
+            parentIndex = currentIndex % 2 == 0 ? (currentIndex - 2) / 2 : (currentIndex - 1) / 2;
         }
     }
 
     public int sink() {
-        left = root * 2 + 1;
-        right = root * 2 + 2;
-        heap[root] = heap[k];
-        int swapIndex = heap[left] > heap[right] ? right : left;
-        if (heap[root] > heap[swapIndex]) {
-            int temp = heap[root];
-            heap[root] = heap[swapIndex];
-            heap[swapIndex] = temp;
-        }
-        k = k - 1;
         return heap[root];
     }
 
-    public static void main(String[] args){
-        ArrayHeap arrayHeap = new ArrayHeap(5);
-        arrayHeap.bubble(5);
-        arrayHeap.bubble(8);
-        arrayHeap.bubble(1);
-        arrayHeap.bubble(3);
-        arrayHeap.bubble(4);
-        System.out.println(arrayHeap.sink());
+    public void swap(int a, int b) {
+        int temp = heap[a];
+        heap[a] = heap[b];
+        heap[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        ArrayHeap arrayHeap = new ArrayHeap(6);
+        arrayHeap.add(5);
+        arrayHeap.add(8);
+        arrayHeap.add(1);
+        arrayHeap.add(4);
+        arrayHeap.add(3);
         System.out.println(arrayHeap.sink());
     }
 }
