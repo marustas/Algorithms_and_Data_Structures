44ebfed38ca8adc80782662ca284a6bbdfdb5b0a
Stanislau Marudau <marudov.stas@gmail.com>
2023-10-13 11:49:31 +0200

    Probably final version of the benchmark

--- heaps/src/test/java/ArrayHeapBenchmark.java
+++ heaps/src/test/java/ArrayHeapBenchmark.java
@@ -3,14 +3,22 @@ import by.example.ArrayHeap;
 import java.util.Random;
 
 public class ArrayHeapBenchmark {
+    private static int[] createArray(int n, int bound) {
+        Random rnd = new Random();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = rnd.nextInt(10, bound);
+        }
+        return array;
+    }
+
     public static void main(String[] args) {
-        int size = 1023;
+        int[] sizes = {100, 200, 400, 800, 1600};
         int bound = 100_000;
         int accuracy = 10_000;
-        int[] operations = {100, 200, 400, 800, 1600};
-        System.out.println("Push Number\t\tPush operation\t\tEnqueue+Dequeue operation\t\tRatio\t\tDepth");
-        int attempt = 0;
-        for (int i = 0; i < operations.length; i++) {
+        int operations = 1000;
+        System.out.println("Size\t\tPush operation\t\tEnqueue+Dequeue operation\t\tRatio\t\tDepth");
+        for (int size : sizes) {
             double min1 = Double.POSITIVE_INFINITY;
             double min2 = Double.POSITIVE_INFINITY;
             int maxDepth = 0;
@@ -20,41 +28,39 @@ public class ArrayHeapBenchmark {
                 maxDepth = 0;
                 Random random = new Random();
 
-                ArrayHeap arrayHeap1 = new ArrayHeap(1023);
-                ArrayHeap arrayHeap2 = new ArrayHeap(1023);
+                int[] increments = createArray(operations, 100);
+                ArrayHeap arrayHeap1 = new ArrayHeap(size);
+                ArrayHeap arrayHeap2 = new ArrayHeap(size);
                 for (int t = 0; t < size; t++) {
                     int item = random.nextInt(bound);
                     arrayHeap1.add(item);
                     arrayHeap2.add(item);
                 }
 
-                for (int operation : operations) {
-                    double start1 = System.nanoTime();
-                    for (int o = 0; o < operation; o++) {
-                        int depth = arrayHeap1.push(random.nextInt(10, 100));
-                        if (depth > maxDepth) {
-                            maxDepth = depth;
-                        }
-                    }
-                    double time1 = System.nanoTime() - start1;
-                    if (time1 < min1) {
-                        min1 = time1;
-                    }
-
-                    double start2 = System.nanoTime();
-                    for (int o = 0; o < operation; o++) {
-                        int removedItem = arrayHeap2.remove();
-                        removedItem += random.nextInt(10, 100);
-                        arrayHeap2.add(removedItem);
-                    }
-                    double time2 = System.nanoTime() - start2;
-                    if (time2 < min2) {
-                        min2 = time2;
+                double start1 = System.nanoTime();
+                for (int o = 0; o < operations; o++) {
+                    int depth = arrayHeap1.push(increments[o]);
+                    if (depth > maxDepth) {
+                        maxDepth = depth;
                     }
                 }
+                double time1 = System.nanoTime() - start1;
+                if (time1 < min1) {
+                    min1 = time1;
+                }
+
+                double start2 = System.nanoTime();
+                for (int o = 0; o < operations; o++) {
+                    int removedItem = arrayHeap2.remove();
+                    removedItem += increments[o];
+                    arrayHeap2.add(removedItem);
+                }
+                double time2 = System.nanoTime() - start2;
+                if (time2 < min2) {
+                    min2 = time2;
+                }
             }
-            System.out.printf("%d \t\t\t %.2f \t\t\t\t\t %.2f \t\t\t\t\t %.2f\t\t%d\n", operations[attempt], min1 / 1000, min2 / 1000, min1 / min2, maxDepth);
-            attempt++;
+            System.out.printf("%d \t\t\t %.2f \t\t\t\t\t %.2f \t\t\t\t\t %.2f\t\t%d\n", size, min1 / 1000, min2 / 1000, min1 / min2, maxDepth);
         }
     }
 }
--- heaps/src/test/java/ListHeapBenchmark.java
+++ heaps/src/test/java/ListHeapBenchmark.java
@@ -3,14 +3,22 @@ import by.example.ListHeap;
 import java.util.Random;
 
 public class ListHeapBenchmark {
+    private static int[] createArray(int n, int bound) {
+        Random rnd = new Random();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = rnd.nextInt(10, bound);
+        }
+        return array;
+    }
+
     public static void main(String[] args) {
-        int size = 1023;
+        int[] sizes = {100, 200, 400, 800, 1600};
         int bound = 100_000;
         int accuracy = 10_000;
-        int[] operations = {100, 200, 400, 800, 1600};
-        System.out.println("Push number\t\tPush operation\t\tEnqueue+Dequeue operation\t\tRatio\t\tDepth");
-        int attempt = 1;
-        for (int i = 0; i < operations.length; i++) {
+        int operations = 1000;
+        System.out.println("Size\t\tPush operation\t\tEnqueue+Dequeue operation\t\tRatio\t\tDepth");
+        for (int size : sizes) {
             double min1 = Double.POSITIVE_INFINITY;
             double min2 = Double.POSITIVE_INFINITY;
             int maxDepth = 0;
@@ -19,7 +27,7 @@ public class ListHeapBenchmark {
                 min2 = Double.POSITIVE_INFINITY;
                 maxDepth = 0;
                 Random random = new Random();
-
+                int[] increments = createArray(operations, 100);
                 ListHeap listHeap1 = new ListHeap();
                 ListHeap listHeap2 = new ListHeap();
                 for (int t = 0; t < size; t++) {
@@ -28,33 +36,30 @@ public class ListHeapBenchmark {
                     listHeap2.enqueue(item);
                 }
 
-                for (int operation : operations) {
-                    double start1 = System.nanoTime();
-                    for (int o = 0; o < operation; o++) {
-                        int depth = listHeap1.push(random.nextInt(10, 100));
-                        if (depth > maxDepth) {
-                            maxDepth = depth;
-                        }
-                    }
-                    double time1 = System.nanoTime() - start1;
-                    if (time1 < min1) {
-                        min1 = time1;
-                    }
-
-                    double start2 = System.nanoTime();
-                    for (int o = 0; o < operation; o++) {
-                        int removedItem = listHeap2.dequeue();
-                        removedItem += random.nextInt(10, 100);
-                        listHeap2.enqueue(removedItem);
-                    }
-                    double time2 = System.nanoTime() - start2;
-                    if (time2 < min2) {
-                        min2 = time2;
+                double start1 = System.nanoTime();
+                for (int o = 0; o < operations; o++) {
+                    int depth = listHeap1.push(increments[o]);
+                    if (depth > maxDepth) {
+                        maxDepth = depth;
                     }
                 }
+                double time1 = System.nanoTime() - start1;
+                if (time1 < min1) {
+                    min1 = time1;
+                }
+
+                double start2 = System.nanoTime();
+                for (int o = 0; o < operations; o++) {
+                    int removedItem = listHeap2.dequeue();
+                    removedItem += increments[o];
+                    listHeap2.enqueue(removedItem);
+                }
+                double time2 = System.nanoTime() - start2;
+                if (time2 < min2) {
+                    min2 = time2;
+                }
             }
-            System.out.printf("%d \t\t\t %.2f \t\t\t\t\t %.2f \t\t\t\t\t %.2f\t\t%d\n", operations[attempt], min1 / 1000, min2 / 1000, min1 / min2, maxDepth);
-            attempt++;
+            System.out.printf("%d \t\t\t %.2f \t\t\t\t\t %.2f \t\t\t\t\t %.2f\t\t%d\n", size, min1 / 1000, min2 / 1000, min1 / min2, maxDepth);
         }
     }
 }
