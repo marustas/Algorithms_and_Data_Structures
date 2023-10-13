7559c815aab071c71d9066c0b0486216c6c347d3
Stanislau Marudau <marudov.stas@gmail.com>
2023-10-13 11:29:27 +0200

    Improved the benchmark for the heaps

--- heaps/src/test/java/ArrayHeapBenchmark.java
+++ heaps/src/test/java/ArrayHeapBenchmark.java
@@ -6,57 +6,57 @@ public class ArrayHeapBenchmark {
     public static void main(String[] args) {
         int size = 1023;
         int bound = 100_000;
-        int tries = 10;
-        int accuracy = 100_000;
-        int operations = 1000;
-        System.out.println("Attempt\t\tPush operation\t\tEnqueue+Dequeue operation\t\tRatio\t\t depth");
+        int accuracy = 10_000;
+        int[] operations = {100, 200, 400, 800, 1600};
+        System.out.println("Attempt\t\tPush operation\t\tEnqueue+Dequeue operation\t\tRatio\t\tDepth");
         int attempt = 1;
-        for (int i = 0; i < tries; i++) {
+        for (int i = 0; i < operations.length; i++) {
             double min1 = Double.POSITIVE_INFINITY;
             double min2 = Double.POSITIVE_INFINITY;
             int maxDepth = 0;
             for (int j = 0; j < accuracy; j++) {
                 min1 = Double.POSITIVE_INFINITY;
                 min2 = Double.POSITIVE_INFINITY;
+                maxDepth = 0;
                 Random random = new Random();
 
-                ArrayHeap arrayHeap1 = new ArrayHeap(size);
+                ArrayHeap arrayHeap1 = new ArrayHeap(1023);
                 for (int t = 0; t < size; t++) {
                     int item = random.nextInt(bound);
                     arrayHeap1.add(item);
                 }
 
-                double start1 = System.nanoTime();
-                for (int o = 0; o < operations; o++) {
-                    int depth = arrayHeap1.push(random.nextInt(10, 100));
-                    if (depth > maxDepth) {
-                        maxDepth = depth;
-                    }
-                }
-                double time1 = System.nanoTime() - start1;
-
-                if (time1 < min1) {
-                    min1 = time1;
-                }
-
-                ArrayHeap arrayHeap2 = new ArrayHeap(size);
+                ArrayHeap arrayHeap2 = new ArrayHeap(1023);
                 for (int t = 0; t < size; t++) {
                     int item = random.nextInt(100_000);
                     arrayHeap2.add(item);
                 }
+                for (int operation : operations) {
+                    double start1 = System.nanoTime();
+                    for (int o = 0; o < operation; o++) {
+                        int depth = arrayHeap1.push(random.nextInt(10, 100));
+                        if (depth > maxDepth) {
+                            maxDepth = depth;
+                        }
+                    }
+                    double time1 = System.nanoTime() - start1;
+                    if (time1 < min1) {
+                        min1 = time1;
+                    }
 
-                double start2 = System.nanoTime();
-                for (int o = 0; o < operations; o++) {
-                    int removedItem = arrayHeap2.remove();
-                    arrayHeap2.add(removedItem + random.nextInt(10, 100));
-                }
-                double time2 = System.nanoTime() - start2;
-
-                if (time2 < min2) {
-                    min2 = time2;
+                    double start2 = System.nanoTime();
+                    for (int o = 0; o < operation; o++) {
+                        int removedItem = arrayHeap2.remove();
+                        removedItem += random.nextInt(10, 100);
+                        arrayHeap2.add(removedItem);
+                    }
+                    double time2 = System.nanoTime() - start2;
+                    if (time2 < min2) {
+                        min2 = time2;
+                    }
                 }
             }
-            System.out.printf("#%d \t\t\t %.2f \t\t\t\t\t %.2f \t\t\t\t\t %.2f\t\t %d\n", attempt, min1 / 1000, min2 / 1000, min1 / min2, maxDepth);
+            System.out.printf("#%d \t\t\t %.2f \t\t\t\t\t %.2f \t\t\t\t\t %.2f\t\t%d\n", attempt, min1 / 1000, min2 / 1000, min1 / min2, maxDepth);
             attempt++;
         }
     }
--- heaps/src/test/java/ListHeapBenchmark.java
+++ heaps/src/test/java/ListHeapBenchmark.java
@@ -6,12 +6,11 @@ public class ListHeapBenchmark {
     public static void main(String[] args) {
         int size = 1023;
         int bound = 100_000;
-        int tries = 10;
-        int accuracy = 100_000;
-        int operations = 1000;
+        int accuracy = 10_000;
+        int[] operations = {100, 200, 400, 800, 1600};
         System.out.println("Attempt\t\tPush operation\t\tEnqueue+Dequeue operation\t\tRatio\t\tDepth");
         int attempt = 1;
-        for (int i = 0; i < tries; i++) {
+        for (int i = 0; i < operations.length; i++) {
             double min1 = Double.POSITIVE_INFINITY;
             double min2 = Double.POSITIVE_INFINITY;
             int maxDepth = 0;
@@ -27,35 +26,34 @@ public class ListHeapBenchmark {
                     listHeap1.enqueue(item);
                 }
 
-                double start1 = System.nanoTime();
-                for (int o = 0; o < operations; o++) {
-                    int depth = listHeap1.push(random.nextInt(10, 100));
-                    if (depth > maxDepth) {
-                        maxDepth = depth;
-                    }
-                }
-                double time1 = System.nanoTime() - start1;
-
-                if (time1 < min1) {
-                    min1 = time1;
-                }
-
                 ListHeap listHeap2 = new ListHeap();
                 for (int t = 0; t < size; t++) {
                     int item = random.nextInt(100_000);
                     listHeap2.enqueue(item);
                 }
+                for (int operation : operations) {
+                    double start1 = System.nanoTime();
+                    for (int o = 0; o < operation; o++) {
+                        int depth = listHeap1.push(random.nextInt(10, 100));
+                        if (depth > maxDepth) {
+                            maxDepth = depth;
+                        }
+                    }
+                    double time1 = System.nanoTime() - start1;
+                    if (time1 < min1) {
+                        min1 = time1;
+                    }
 
-                double start2 = System.nanoTime();
-                for (int o = 0; o < operations; o++) {
-                    int removedItem = listHeap2.dequeue();
-                    removedItem += random.nextInt(10, 100);
-                    listHeap2.enqueue(removedItem);
-                }
-                double time2 = System.nanoTime() - start2;
-
-                if (time2 < min2) {
-                    min2 = time2;
+                    double start2 = System.nanoTime();
+                    for (int o = 0; o < operation; o++) {
+                        int removedItem = listHeap2.dequeue();
+                        removedItem += random.nextInt(10, 100);
+                        listHeap2.enqueue(removedItem);
+                    }
+                    double time2 = System.nanoTime() - start2;
+                    if (time2 < min2) {
+                        min2 = time2;
+                    }
                 }
             }
             System.out.printf("#%d \t\t\t %.2f \t\t\t\t\t %.2f \t\t\t\t\t %.2f\t\t%d\n", attempt, min1 / 1000, min2 / 1000, min1 / min2, maxDepth);
