d01f4b3800a3906f8e91eba23ac9ddb84862c0b8
Stanislau Marudau <marudov.stas@gmail.com>
2023-10-23 09:44:13 +0200

    Implmented a benchmark for naive solution to finding the shortest path

--- graphs/src/main/java/by/example/Naive.java
+++ graphs/src/main/java/by/example/Naive.java
@@ -3,13 +3,27 @@ package by.example;
 public class Naive {
     public static void main(String[] args) {
         Map map = new Map("graphs/src/main/java/by/example/trains.csv");
-        String from = args[0];
-        String to = args[1];
-        Integer max = Integer.valueOf(args[2]);
-        long t0 = System.nanoTime();
-        Integer dist = shortest(map.lookup(from), map.lookup(to), max);
-        long time = (System.nanoTime() - t0) / 1_000_000;
-        System.out.println("shortest: " + dist + " min (" + time + " ms)");
+        String[] cityPairs = {
+                "Malmö,Göteborg,300",
+                "Göteborg,Stockholm,300",
+                "Malmö,Stockholm,300",
+                "Stockholm,Sundsvall,300",
+                "Stockholm,Umeå,300",
+                "Göteborg,Sundsvall,300",
+                "Sundsvall,Umeå,300",
+                "Umeå,Gvteborg,300",
+                "Göteborg,Umeå,300"
+        };
+        for (String cityPair : cityPairs) {
+            String[] cities = cityPair.split(",");
+            String from = cities[0];
+            String to = cities[1];
+            Integer max = Integer.valueOf(args[2]);
+            long t0 = System.nanoTime();
+            Integer dist = shortest(map.lookup(from), map.lookup(to), max);
+            long time = (System.nanoTime() - t0) / 1_000_000;
+            System.out.println("shortest: " + dist + " min (" + time + " ms)");
+        }
     }
 
     private static Integer shortest(Map.City from, Map.City to, Integer max) {
